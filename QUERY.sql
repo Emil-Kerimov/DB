ca93e9047ef90956e9b6ca13752255b896e18503
Emil Kerimov <emilkerimovofficial@gmail.com>
2025-04-01 01:56:32 +0300

    pr2

--- QUERY.sql
+++ QUERY.sql
@@ -4,12 +4,65 @@ SELECT * FROM Wallpaper;
 -- Вибірка всіх клієнтів
 SELECT * FROM Customer;
 
+-- Вибірка всіх  Orders
+SELECT * FROM Orders;
+
+-- Вибірка всіх клієнтів
+SELECT * FROM WallpaperOrders;
+
 -- Вибірка всіх замовлень із датою та кількістю
 SELECT Order_ID, OrderDate, Quantity FROM Orders;
 
 -- Пошук шпалер із ціною більше 15
 SELECT Name, Price FROM Wallpaper
 WHERE Price > 15;
+
+-- Пошук шпалер із ціною більше 15 і яких менше 10 у наявності
+SELECT Name, Price FROM Wallpaper
+WHERE Price > 15 AND Stock < 10;
+
+
+-- Пошук шпалер типу Floral
+SELECT Name, Price FROM Wallpaper
+WHERE Name LIKE '%Floral%';
+
+-- Показати всі замовлення з Email клієнтів
+SELECT o.Order_ID, c.Email, o.OrderDate
+FROM Orders o
+JOIN Customer c ON o.Customer_ID = c.Customer_ID;
+
+-- Показати всі шпалери, навіть якщо їх ніхто не замовляв
+SELECT w.Name, wo.Order_ID
+FROM Wallpaper w
+LEFT JOIN WallpaperOrders wo ON w.Wallpaper_ID = wo.Wallpaper_ID;
+
+-- Пошук шпалер із ціною більше AVG
+SELECT Name, Price FROM Wallpaper
+WHERE Price > (SELECT AVG(Price) FROM Wallpaper);
+
+SELECT C.Email, COUNT(O.Order_ID) AS TotalOrders
+FROM Customer C
+JOIN Orders O ON C.Customer_ID = O.Customer_ID
+GROUP BY C.Email
+HAVING COUNT(O.Order_ID) > 1;
+
+-- Отримання даних про замовлення, клієнтів і шпалери з підрахунком загальної суми кожного замовлення
+SELECT O.Order_ID, O.OrderDate, C.Email, SUM(W.Price * WO.Quantity) AS TotalPrice
+FROM Orders O
+JOIN Customer C ON O.Customer_ID = C.Customer_ID
+JOIN WallpaperOrders WO ON O.Order_ID = WO.Order_ID
+JOIN Wallpaper W ON WO.Wallpaper_ID = W.Wallpaper_ID
+GROUP BY O.Order_ID, O.OrderDate, C.Email
+ORDER BY TotalPrice DESC;
+
+--Отримання замовлень клієнтів, зроблених після певної дати
+SELECT O.Order_ID, O.OrderDate, C.Email, W.Name AS Wallpaper_Name, WO.Quantity
+FROM Orders O
+JOIN Customer C ON O.Customer_ID = C.Customer_ID
+JOIN WallpaperOrders WO ON O.Order_ID = WO.Order_ID
+JOIN Wallpaper W ON WO.Wallpaper_ID = W.Wallpaper_ID
+WHERE O.OrderDate > '2023-01-11';
+
 
 -- Сортування шпалер за ціною за спаданням
 SELECT Name, Price FROM Wallpaper
